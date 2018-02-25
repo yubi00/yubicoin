6a5e74760209064e927ae62ddb8dff7f33a25b4b
YUBA RAJ KHADKA <ubrajkhadka@gmail.com>
2018-02-25 13:44:18 +1100

    erc20 token

--- yubicoin.sol
+++ yubicoin.sol
@@ -11,7 +11,7 @@ pragma solidity ^0.4.18;
 //
 // Enjoy.
 //
-// (c) by Moritz Neto with BokkyPooBah / Bok Consulting Pty Ltd Au 2017. The MIT Licence.
+// 
 // ----------------------------------------------------------------------------
 
 
